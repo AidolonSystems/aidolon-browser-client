9c314a91936b23579031b809d21216e1348ad87c
SethTurin <ubotdev@gmail.com>
2025-03-28 02:34:33 -0400

    added browser session

---
+++ example.py
@@ -0,0 +1,18 @@
+from browser import create_session
+
+# Example 1: Using the session without a with block
+browser = create_session()
+browser.navigate("https://www.google.com")
+browser.type("the search input", "donuts")
+browser.press("the search input", "Enter")
+browser.click("the first search result")
+browser.close_session()
+
+print("-----")
+
+# Example 2: Using the session with a with block
+with create_session() as browser:
+    browser.navigate("https://www.google.com")
+    browser.type("the search input", "donuts")
+    browser.press("the search input", "Enter")
+    browser.click("the first search result")
